3bc6c7d114c55f2eab15cfce17e39686f22c6957
unknown <miojodepopoca@gmail.com>
2023-10-26 17:15:35 -0300

    feat: cadastrando alunos

--- alunos/urls.py
+++ alunos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name="index")
+   path('', views.index, name="alunos"),
+   path('cadastrar_aluno/', views.create, name="cadastrar_aluno")
 ]
 
--- alunos/views.py
+++ alunos/views.py
@@ -1,9 +1,21 @@
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
 from .models import Aluno
+from .forms import AlunoForm
 
 # Create your views here.
 def index(request):
     alunos = Aluno.objects.all()
     return render(request, 'index.html', {'alunos': alunos})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('alunos')
+    else:
+        form = AlunoForm()
+    
+    return render(request, 'cadastro.html', {'form': form})
